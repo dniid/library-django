476b585ed7feb9d4883458b1a50b57b5c2abfe14
dniid <dniidtf2@gmail.com>
2022-07-25 10:42:06 -0300

    implemented some basic testing

--- author/tests.py
+++ author/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
-# Create your tests here.
+class UrlTest(TestCase):
+
+    def testAuthorPage(self):
+        response = self.client.get('/author/')
+        print(response)
+
+        self.assertEqual(response.status_code, 200)
--- book/tests.py
+++ book/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
-# Create your tests here.
+class UrlTest(TestCase):
+
+    def testBookPage(self):
+        response = self.client.get('/book/')
+        print(response)
+
+        self.assertEqual(response.status_code, 200)
